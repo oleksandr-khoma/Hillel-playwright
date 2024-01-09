a2023c6ab3349daccacaf27b97f76185be244b95
oleksandr-khoma <d3m11av@gmail.com>
2024-01-09 12:29:58 +0200

    feat: requests

--- pageobjects/GaragePage.js
+++ pageobjects/GaragePage.js
@@ -5,11 +5,16 @@ export default class GaragePage extends BasePage {
   constructor(page) {
     super(page);
     this.settings = page.locator('[routerlink="settings"]');
+    this.profile = page.locator('[routerlink="profile"]');
     this.addCarButton = this.page.locator("button", { hasText: "Add car" });
   }
 
   async openSettings() {
     await this.settings.click();
+  }
+
+  async openProfile() {
+    await this.profile.click();
   }
 
   async openAddCarPopup() {
